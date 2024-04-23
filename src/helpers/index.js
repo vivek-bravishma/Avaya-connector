716ae33b4621eca519bc33516875b3c186f5b793
ViveK <vivekn@bravishma.com>
2024-04-23 16:34:54 +0530

    vonage changes

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,174 +1,212 @@
 import axios from "axios";
+// const { Vonage } = require('@vonage/server-sdk');
+// const { WhatsAppAudio } = require('@vonage/messages');
+import { Vonage } from "@vonage/server-sdk";
+import {
+	WhatsAppText,
+	WhatsAppAudio,
+	WhatsAppImage,
+	WhatsAppFile,
+} from "@vonage/messages";
 
 import avayaConfig from "../config/avaya.js";
 
 const {
-  accountId,
-  client_id,
-  client_secret,
-  channelProviderId,
-  integrationId,
-  integrationName,
-  grant_type,
-  accessTokenUrl,
-  createSubscriptionUrl,
-  sendMsgUrl,
-  callbackUrl,
-  vonageSMSUrl,
-  vonageApiKey,
-  vonageApiSecret,
-  vonageUrl,
+	accountId,
+	client_id,
+	client_secret,
+	channelProviderId,
+	integrationId,
+	integrationName,
+	grant_type,
+	accessTokenUrl,
+	createSubscriptionUrl,
+	sendMsgUrl,
+	callbackUrl,
+	vonageSMSUrl,
+	vonageApiKey,
+	vonageApiSecret,
+	vonageApplicationId,
+	vonagePrivateKey,
+	vonageWhatsAppNumber,
+	vonageUrl,
 } = avayaConfig;
 
-export async function fetchAccessToken() {
-  try {
-    var options = {
-      method: "POST",
-      url: accessTokenUrl,
-      headers: {
-        accept: "application/json",
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      data: {
-        grant_type,
-        client_id,
-        client_secret,
-      },
-    };
+const vonage = new Vonage({
+	apiKey: vonageApiKey,
+	apiSecret: vonageApiSecret,
+	applicationId: vonageApplicationId,
+	privateKey: vonagePrivateKey,
+});
 
-    let resp = await axios.request(options);
-    return resp.data;
-  } catch (error) {
-    if (error.response.data) {
-      console.log(
-        "fetchAccessToken.error.response.data==> ",
-        error.response.data
-      );
-      throw error.response.data;
-    } else {
-      console.log("error.data==> ", error.message);
-      throw error.message;
-    }
-  }
+export async function fetchAccessToken() {
+	try {
+		var options = {
+			method: "POST",
+			url: accessTokenUrl,
+			headers: {
+				accept: "application/json",
+				"Content-Type": "application/x-www-form-urlencoded",
+			},
+			data: {
+				grant_type,
+				client_id,
+				client_secret,
+			},
+		};
+
+		let resp = await axios.request(options);
+		return resp.data;
+	} catch (error) {
+		if (error.response.data) {
+			console.log(
+				"fetchAccessToken.error.response.data==> ",
+				error.response.data
+			);
+			throw error.response.data;
+		} else {
+			console.log("error.data==> ", error.message);
+			throw error.message;
+		}
+	}
 }
 
 export async function createSubscription() {
-  try {
-    let { access_token } = await fetchAccessToken();
-    var options = {
-      method: "POST",
-      url: createSubscriptionUrl,
-      headers: {
-        accept: "application/json",
-        authorization: `Bearer ${access_token}`,
-        "content-type": "application/json",
-      },
-      data: JSON.stringify({
-        eventTypes: ["ALL"],
-        callbackUrl,
-        channelProviderId,
-      }),
-    };
+	try {
+		let { access_token } = await fetchAccessToken();
+		var options = {
+			method: "POST",
+			url: createSubscriptionUrl,
+			headers: {
+				accept: "application/json",
+				authorization: `Bearer ${access_token}`,
+				"content-type": "application/json",
+			},
+			data: JSON.stringify({
+				eventTypes: ["ALL"],
+				callbackUrl,
+				channelProviderId,
+			}),
+		};
 
-    let resp = await axios.request(options);
-    return resp.data;
-  } catch (error) {
-    if (error.response.data) {
-      console.log(
-        "createSubscription.error.response.data==> ",
-        error.response.data
-      );
-      throw error.response.data;
-    } else {
-      console.log("error.data==> ", error.message);
-      throw error.message;
-    }
-  }
+		let resp = await axios.request(options);
+		return resp.data;
+	} catch (error) {
+		if (error.response.data) {
+			console.log(
+				"createSubscription.error.response.data==> ",
+				error.response.data
+			);
+			throw error.response.data;
+		} else {
+			console.log("error.data==> ", error.message);
+			throw error.message;
+		}
+	}
 }
 
 export async function sendMessage(sender, message, mobileNo, channel) {
-  try {
-    let { access_token } = await fetchAccessToken();
+	try {
+		let { access_token } = await fetchAccessToken();
 
-    var options = {
-      method: "POST",
-      url: sendMsgUrl,
-      headers: {
-        accept: "application/json",
-        authorization: `Bearer ${access_token}`,
-        "content-type": "application/json",
-      },
-      data: {
-        customerIdentifiers: { mobile: [mobileNo] },
-        customData: { msngChannel: channel },
-        body: { elementText: { text: message }, elementType: "text" },
-        channelProviderId,
-        channelId: "Messaging",
-        senderName: sender,
-        businessAccountName: integrationId,
-        providerDialogId: channel,
-      },
-    };
+		var options = {
+			method: "POST",
+			url: sendMsgUrl,
+			headers: {
+				accept: "application/json",
+				authorization: `Bearer ${access_token}`,
+				"content-type": "application/json",
+			},
+			data: {
+				customerIdentifiers: { mobile: [mobileNo] },
+				customData: { msngChannel: channel },
+				body: { elementText: { text: message }, elementType: "text" },
+				channelProviderId,
+				channelId: "Messaging",
+				senderName: sender,
+				businessAccountName: integrationId,
+				providerDialogId: channel,
+			},
+		};
 
-    let resp = await axios.request(options);
-    return resp.data;
-  } catch (error) {
-    if (error.response.data) {
-      console.log("sendMessage.error.response.data==> ", error.response.data);
-      throw error.response.data;
-    } else {
-      console.log("error.data sendmessage==> ", error.message);
-      throw error.message;
-    }
-  }
+		let resp = await axios.request(options);
+		return resp.data;
+	} catch (error) {
+		if (error.response.data) {
+			console.log(
+				"sendMessage.error.response.data==> ",
+				error.response.data
+			);
+			throw error.response.data;
+		} else {
+			console.log("error.data sendmessage==> ", error.message);
+			throw error.message;
+		}
+	}
 }
 
 export async function sendSMS(recipiant, replyMsg) {
-  // { from, text, to, api_key, api_secret }
-  try {
-    const payload = {
-      from: "ayva",
-      text: replyMsg,
-      to: recipiant,
-      api_key: vonageApiKey,
-      api_secret: vonageApiSecret,
-    };
-    let resp = await axios.post(vonageSMSUrl, payload);
-    return resp;
-  } catch (error) {
-    console.log("send sms error--", error);
-    throw error;
-  }
+	// { from, text, to, api_key, api_secret }
+	try {
+		const payload = {
+			from: "ayva",
+			text: replyMsg,
+			to: recipiant,
+			api_key: vonageApiKey,
+			api_secret: vonageApiSecret,
+		};
+		let resp = await axios.post(vonageSMSUrl, payload);
+		return resp;
+	} catch (error) {
+		console.log("send sms error--", error);
+		throw error;
+	}
 }
 
 export async function sendVonageMsg(
-  to,
-  text,
-  message_type = "text",
-  channel = "whatsapp"
+	to,
+	text,
+	message_type = "text",
+	channel = "whatsapp"
 ) {
-  try {
-    const payload = {
-      from: "14157386102",
-      to,
-      message_type,
-      text,
-      channel,
-    };
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      auth: {
-        username: "b90a1d65",
-        password: "Xkzmw60T1nLE8hbj",
-      },
-    };
-    let resp = await axios.post(vonageUrl, payload, config);
-    return resp;
-  } catch (error) {
-    console.log("send vonage msg error--> ", error);
-    throw error;
-  }
+	try {
+		const payload = {
+			from: "14157386102",
+			to,
+			message_type,
+			text,
+			channel,
+		};
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+			auth: {
+				username: "b90a1d65",
+				password: "Xkzmw60T1nLE8hbj",
+			},
+		};
+		let resp = await axios.post(vonageUrl, payload, config);
+		return resp;
+	} catch (error) {
+		console.log("send vonage msg error--> ", error);
+		throw error;
+	}
+}
+
+export async function sendVonageWhatsappText(to, text) {
+	try {
+		let resp = await vonage.messages.send(
+			new WhatsAppText({
+				text,
+				to,
+				from: vonageWhatsAppNumber,
+			})
+		);
+		return resp;
+	} catch (error) {
+		console.log("send vonage whatsapp text error--> ", error);
+		throw error;
+	}
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 const app = express();
 
 import avayaConfig from "./config/avaya.js";
+
 import {
-  fetchAccessToken,
-  createSubscription,
-  sendMessage,
-  sendSMS,
-  sendVonageMsg,
+	fetchAccessToken,
+	createSubscription,
+	sendMessage,
+	sendSMS,
+	sendVonageMsg,
 } from "./helpers/index.js";
-
-// async function fu() {
-//   try {
-//     // let tokenResp = await fetchAccessToken();
-//     // let tokenResp = await createSubscription();
-//     let tokenResp = await sendMessage("anonmouse", "kem cho");
-
-//     console.log("resppppppppppppp+.> ", tokenResp);
-//   } catch (error) {
-//     console.log("eeeeeeeeeeeeeeeeeeee========> ", error.detail);
-//   }
-// }
-// fu();
 
 app.use(cors());
 app.use(express.json());
@@ -32,79 +21,83 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("avaya connector server started at ", port));
 
 app.get("/callback", (req, res) => {
-  console.log("callback get request");
-  res.send("callback url working");
+	console.log("callback get request");
+	res.send("callback url working");
 });
 
 app.post("/callback", async (req, res) => {
-  try {
-    const reqBody = req.body;
-    console.log(
-      `======== callback post request ${reqBody.eventType} ${reqBody.senderParticipantType} =========`
-    );
-    if (reqBody.eventType === "MESSAGES") {
-      if (
-        reqBody.senderParticipantType === "AGENT" ||
-        reqBody.senderParticipantType === "SYSTEM"
-      ) {
-        //  send this data to client/customer
-        let replyMsg = reqBody.body.elementText.text;
-        let recipiant = reqBody.recipientParticipants[0].providerParticipantId;
-        console.log("Recipient=> ", recipiant, "  replyMsg=> ", replyMsg);
-        if (recipiant && replyMsg) {
-          if (reqBody.providerDialogId === "whatsapp") {
-            let vonageResp = await sendVonageMsg(
-              recipiant,
-              replyMsg,
-              "text",
-              "whatsapp"
-            );
-            console.log("vonage resp--> ", vonageResp.data);
-          } else {
-            let smsResp = await sendSMS(recipiant, replyMsg);
-            console.log("sms resp--> ", smsResp.data);
-          }
-        }
-      } else if (reqBody.senderParticipantType === "CUSTOMER") {
-        console.log("customer msg --> ", reqBody.body.elementText.text);
-      }
-    }
-    res.send("callback url working");
-  } catch (error) {
-    console.log("error in callback---> ", error);
-    res.send(error);
-  }
+	try {
+		const reqBody = req.body;
+		console.log(
+			`======== callback post request ${reqBody.eventType} ${reqBody.senderParticipantType} =========`
+		);
+		if (reqBody.eventType === "MESSAGES") {
+			if (
+				reqBody.senderParticipantType === "AGENT" ||
+				reqBody.senderParticipantType === "SYSTEM"
+			) {
+				//  send this data to client/customer
+				let replyMsg = reqBody.body.elementText.text;
+				let recipiant =
+					reqBody.recipientParticipants[0].providerParticipantId;
+				console.log(
+					"Recipient=> ",
+					recipiant,
+					"  replyMsg=> ",
+					replyMsg
+				);
+				if (recipiant && replyMsg) {
+					if (reqBody.providerDialogId === "whatsapp") {
+						let vonageResp = await sendVonageWhatsappText(
+							recipiant,
+							replyMsg
+						);
+						console.log("vonage resp--> ", vonageResp.data);
+					} else {
+						let smsResp = await sendSMS(recipiant, replyMsg);
+						console.log("sms resp--> ", smsResp.data);
+					}
+				}
+			} else if (reqBody.senderParticipantType === "CUSTOMER") {
+				console.log("customer msg --> ", reqBody.body.elementText.text);
+			}
+		}
+		res.send("callback url working");
+	} catch (error) {
+		console.log("error in callback---> ", error);
+		res.send(error);
+	}
 });
 
 app.post("/send-message", async (req, res) => {
-  console.log("send message called");
-  try {
-    let { sender, message, mobileNo, channel } = req.body;
-    console.log(sender, message, mobileNo);
-    let tokenResp = await sendMessage(sender, message, mobileNo, channel);
-    res.send(tokenResp);
-  } catch (error) {
-    console.log("eeeeeeeeeeeeeeeeeeee========> ", error.detail);
-    res.send(error);
-  }
+	console.log("send message called");
+	try {
+		let { sender, message, mobileNo, channel } = req.body;
+		console.log(sender, message, mobileNo);
+		let tokenResp = await sendMessage(sender, message, mobileNo, channel);
+		res.send(tokenResp);
+	} catch (error) {
+		console.log("eeeeeeeeeeeeeeeeeeee========> ", error.detail);
+		res.send(error);
+	}
 });
 
 app.get("/vonage-callback", (req, res) => {
-  console.log("GET vonage-callback");
-  console.log(req.body);
-  res.send("vonage-callback working");
+	console.log("GET vonage-callback");
+	console.log(req.body);
+	res.send("vonage-callback working");
 });
 
 app.post("/vonage-callback", async (req, res) => {
-  console.log("POST vonage-callback");
-  console.log(req.body);
-  try {
-    let { profile, text, from, channel } = req.body;
-    console.log(profile.name, text, from, channel);
-    let tokenResp = await sendMessage(profile.name, text, from, channel);
-    res.send(tokenResp);
-  } catch (error) {
-    console.log("eeeeeeeeeeeeeeeeeeee========> ", error.detail);
-    res.send(error);
-  }
+	console.log("POST vonage-callback");
+	console.log(req.body);
+	try {
+		let { profile, text, from, channel } = req.body;
+		console.log(profile.name, text, from, channel);
+		let tokenResp = await sendMessage(profile.name, text, from, channel);
+		res.send(tokenResp);
+	} catch (error) {
+		console.log("eeeeeeeeeeeeeeeeeeee========> ", error.detail);
+		res.send(error);
+	}
 });
