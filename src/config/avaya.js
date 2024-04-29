a4070c26fa2d176a00fda70d9befc9dbcf8fb87a
ViveK <vivekn@bravishma.com>
2024-04-29 13:28:05 +0530

    prettier formatting

--- src/config/avaya.js
+++ src/config/avaya.js
@@ -1,39 +1,39 @@
 let config = {
-  accountId: "MCTOTJ",
-  client_id: "clientApp1",
-  client_secret: "idgP2WRZf3eHsPTdUkgdOQlXoxjaMJZZ",
-  channelProviderId: "4e287bdf-8c96-4eae-ac46-1ff091bcbec0",
-  integrationId: "c082f77d-3ae2-4e4c-8126-65db09b33767",
-  integrationName: "SECUSTOM",
-  grant_type: "client_credentials",
-  avayaBaseUrl: "https://experience.api.avayacloud.com",
-};
+	accountId: 'MCTOTJ',
+	client_id: 'clientApp1',
+	client_secret: 'idgP2WRZf3eHsPTdUkgdOQlXoxjaMJZZ',
+	channelProviderId: '4e287bdf-8c96-4eae-ac46-1ff091bcbec0',
+	integrationId: 'c082f77d-3ae2-4e4c-8126-65db09b33767',
+	integrationName: 'SECUSTOM',
+	grant_type: 'client_credentials',
+	avayaBaseUrl: 'https://experience.api.avayacloud.com',
+}
 
 config = {
-  ...config,
-  accessTokenUrl: `${config.avayaBaseUrl}/api/auth/v1/${config.accountId}/protocol/openid-connect/token`,
-  createSubscriptionUrl: `${config.avayaBaseUrl}/api/digital/webhook/v1/accounts/${config.accountId}/subscriptions`,
-  sendMsgUrl: `${config.avayaBaseUrl}/api/digital/custom-messaging/v1/accounts/${config.accountId}/messages`,
-  avayaFileUploadUrl: `${config.avayaBaseUrl}/api/media-store/v1/accounts/${config.accountId}/media/signed-upload-uri`,
-  callbackUrl: "https://connector.lab.bravishma.com/callback",
-  vonageSMSUrl: "https://rahul.lab.bravishma.com/send-vonage-sms",
-  vonageApiKey: process.env.VONAGE_API_KEY,
-  vonageApiSecret: process.env.VONAGE_API_SECRET,
-  vonageApplicationId: process.env.VONAGE_APPLICATION_ID,
-  vonagePrivateKey: process.env.VONAGE_PRIVATE_KEY,
-  vonageWhatsAppNumber: "14157386102",
-  vonageUrl: "https://messages-sandbox.nexmo.com/v1/messages",
-  vonage_BASE_URL: "https://messages-sandbox.nexmo.com",
-  // VIBER_SERVICE_MESSAGE_ID: "16273",
-  VIBER_SERVICE_MESSAGE_ID: "22353",
-  lineMessageUrl: " https://api.line.me/v2/bot/message/push",
-  lineToken:
-    "7vm3N0qNPBv2aSC+2aYvz/++w53S57kFVIC/72IQeIR4AxfX/kmO+3Sm4JSMNL893waGJcfsCrGzwt6jkw9OAo4oorjfxzLFcMO/mHmMIuufiCXQifHokpwDvgcuTUq8JCx/4KqsBYKPsKuk+UZtzwdB04t89/1O/w1cDnyilFU=",
-};
+	...config,
+	accessTokenUrl: `${config.avayaBaseUrl}/api/auth/v1/${config.accountId}/protocol/openid-connect/token`,
+	createSubscriptionUrl: `${config.avayaBaseUrl}/api/digital/webhook/v1/accounts/${config.accountId}/subscriptions`,
+	sendMsgUrl: `${config.avayaBaseUrl}/api/digital/custom-messaging/v1/accounts/${config.accountId}/messages`,
+	avayaFileUploadUrl: `${config.avayaBaseUrl}/api/media-store/v1/accounts/${config.accountId}/media/signed-upload-uri`,
+	callbackUrl: 'https://connector.lab.bravishma.com/callback',
+	vonageSMSUrl: 'https://rahul.lab.bravishma.com/send-vonage-sms',
+	vonageApiKey: process.env.VONAGE_API_KEY,
+	vonageApiSecret: process.env.VONAGE_API_SECRET,
+	vonageApplicationId: process.env.VONAGE_APPLICATION_ID,
+	vonagePrivateKey: process.env.VONAGE_PRIVATE_KEY,
+	vonageWhatsAppNumber: '14157386102',
+	vonageUrl: 'https://messages-sandbox.nexmo.com/v1/messages',
+	vonage_BASE_URL: 'https://messages-sandbox.nexmo.com',
+	// VIBER_SERVICE_MESSAGE_ID: "16273",
+	VIBER_SERVICE_MESSAGE_ID: '22353',
+	lineMessageUrl: ' https://api.line.me/v2/bot/message/push',
+	lineToken:
+		'7vm3N0qNPBv2aSC+2aYvz/++w53S57kFVIC/72IQeIR4AxfX/kmO+3Sm4JSMNL893waGJcfsCrGzwt6jkw9OAo4oorjfxzLFcMO/mHmMIuufiCXQifHokpwDvgcuTUq8JCx/4KqsBYKPsKuk+UZtzwdB04t89/1O/w1cDnyilFU=',
+}
 
 // export default {
 
 //   avayaBaseUrl: "https://experience.api.avayacloud.com",
 // };
 
-export default config;
+export default config
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-const app = express();
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+const app = express()
 
 // import avayaConfig from "./config/avaya.js";
 // const {
@@ -26,300 +26,316 @@ const app = express();
 // } = avayaConfig;
 
 import {
-  fetchAccessToken,
-  createSubscription,
-  sendMessage,
-  sendSMS,
-  sendVonageMsg,
-  sendVonageWhatsappText,
-  sendVonageWhatsappImage,
-  uploadFileToAvaya,
-  uploadImage,
-  sendVonageWhatsappFile,
-  sendLineTextMessage,
-  sendLineImageMessage,
-  sendVonageViberText,
-} from "./helpers/index.js";
+	fetchAccessToken,
+	createSubscription,
+	sendMessage,
+	sendSMS,
+	sendVonageMsg,
+	sendVonageWhatsappText,
+	sendVonageWhatsappImage,
+	uploadFileToAvaya,
+	uploadImage,
+	sendVonageWhatsappFile,
+	sendLineTextMessage,
+	sendLineImageMessage,
+	sendVonageViberText,
+} from './helpers/index.js'
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(
-    "============================================================================================="
-  );
-  console.log("avaya connector server started at ", port);
-});
+	console.log(
+		'============================================================================================='
+	)
+	console.log('avaya connector server started at ', port)
+})
 
-app.get("/test", async (req, res) => {
-  res.send("api working");
-});
+app.get('/test', async (req, res) => {
+	res.send('api working')
+})
 
 // callback for avaya
-app.post("/callback", async (req, res) => {
-  try {
-    const reqBody = req.body;
-    console.log(
-      `======== callback post request ${reqBody.eventType} ${reqBody.senderParticipantType} =========`
-    );
-    if (reqBody.eventType === "MESSAGES") {
-      if (
-        reqBody.senderParticipantType === "AGENT" ||
-        reqBody.senderParticipantType === "SYSTEM"
-      ) {
-        //  send this data to client/customer
-        let replyMsg = reqBody.body.elementText.text;
-        let recipiant = reqBody.recipientParticipants[0].providerParticipantId;
-        console.log(
-          "Recipient=> ",
-          recipiant,
-          "  replyMsg=> ",
-          replyMsg,
-          "   providerDialogId=> ",
-          reqBody.providerDialogId
-        );
-        // console.log(reqBody);
-        if (recipiant) {
-          if (reqBody.providerDialogId === "whatsapp") {
-            let type = reqBody.body.elementType;
-            console.log("\n\n\nWhatsapp message type : ", type);
-            if (type === "image") {
-              let imageUrl = reqBody.attachments[0].url;
-              let vonageResp = await sendVonageWhatsappImage(
-                recipiant,
-                imageUrl
-              );
-              console.log("vonage image resp--> ", vonageResp.data);
-            } else if (type === "file") {
-              let fileUrl = reqBody.attachments[0].url;
-              let vonageResp = await sendVonageWhatsappFile(recipiant, fileUrl);
-              console.log("vonage file resp--> ", vonageResp.data);
-            } else {
-              let vonageResp = await sendVonageWhatsappText(
-                recipiant,
-                replyMsg
-              );
-              console.log("vonage resp--> ", vonageResp.data);
-            }
-          } else if (reqBody.providerDialogId === "Line") {
-            console.log("Handle Line messages here");
-            let type = reqBody.body.elementType;
-            console.log("\n\nLine message type : ", type);
+app.post('/callback', async (req, res) => {
+	try {
+		const reqBody = req.body
+		console.log(
+			`======== callback post request ${reqBody.eventType} ${reqBody.senderParticipantType} =========`
+		)
+		if (reqBody.eventType === 'MESSAGES') {
+			if (
+				reqBody.senderParticipantType === 'AGENT' ||
+				reqBody.senderParticipantType === 'SYSTEM'
+			) {
+				//  send this data to client/customer
+				let replyMsg = reqBody.body.elementText.text
+				let recipiant =
+					reqBody.recipientParticipants[0].providerParticipantId
+				console.log(
+					'Recipient=> ',
+					recipiant,
+					'  replyMsg=> ',
+					replyMsg,
+					'   providerDialogId=> ',
+					reqBody.providerDialogId
+				)
+				// console.log(reqBody);
+				if (recipiant) {
+					if (reqBody.providerDialogId === 'whatsapp') {
+						let type = reqBody.body.elementType
+						console.log('\n\n\nWhatsapp message type : ', type)
+						if (type === 'image') {
+							let imageUrl = reqBody.attachments[0].url
+							let vonageResp = await sendVonageWhatsappImage(
+								recipiant,
+								imageUrl
+							)
+							console.log(
+								'vonage image resp--> ',
+								vonageResp.data
+							)
+						} else if (type === 'file') {
+							let fileUrl = reqBody.attachments[0].url
+							let vonageResp = await sendVonageWhatsappFile(
+								recipiant,
+								fileUrl
+							)
+							console.log('vonage file resp--> ', vonageResp.data)
+						} else {
+							let vonageResp = await sendVonageWhatsappText(
+								recipiant,
+								replyMsg
+							)
+							console.log('vonage resp--> ', vonageResp.data)
+						}
+					} else if (reqBody.providerDialogId === 'Line') {
+						console.log('Handle Line messages here')
+						let type = reqBody.body.elementType
+						console.log('\n\nLine message type : ', type)
 
-            if (type === "text") {
-              let lineResponse = await sendLineTextMessage(
-                recipiant,
-                replyMsg,
-                type,
-                reqBody.providerDialogId
-              );
-            } else if (type === "image") {
-              let imageUrl = reqBody.attachments[0].url;
-              let lineResponse = await sendLineImageMessage(
-                recipiant,
-                imageUrl,
-                type,
-                reqBody.providerDialogId
-              );
-            }
-          } else if (reqBody.providerDialogId === "viber_service") {
-            let type = reqBody.body.elementType;
-            console.log("\n\n\n viber message type : ", type);
-            if (type === "image") {
-              let imageUrl = reqBody.attachments[0].url;
-              let vonageResp = await sendVonageWhatsappImage(
-                recipiant,
-                imageUrl
-              );
-              console.log("vonage image resp--> ", vonageResp.data);
-            } else if (type === "file") {
-              let fileUrl = reqBody.attachments[0].url;
-              let vonageResp = await sendVonageWhatsappFile(recipiant, fileUrl);
-              console.log("vonage file resp--> ", vonageResp.data);
-            } else {
-              let vonageResp = await sendVonageViberText(recipiant, replyMsg);
-              console.log("vonage resp--> ", vonageResp.data);
-            }
-          } else {
-            // let smsResp = await sendSMS(recipiant, replyMsg);
-            // console.log("sms resp--> ", smsResp.data);
-          }
-        }
-      } else if (reqBody.senderParticipantType === "CUSTOMER") {
-        console.log("customer msg --> ", reqBody.body.elementText.text);
-      }
-    }
-    res.send("callback url working");
-  } catch (error) {
-    console.log("error in callback---> ", error);
-    res.send(error);
-  }
-});
+						if (type === 'text') {
+							let lineResponse = await sendLineTextMessage(
+								recipiant,
+								replyMsg,
+								type,
+								reqBody.providerDialogId
+							)
+						} else if (type === 'image') {
+							let imageUrl = reqBody.attachments[0].url
+							let lineResponse = await sendLineImageMessage(
+								recipiant,
+								imageUrl,
+								type,
+								reqBody.providerDialogId
+							)
+						}
+					} else if (reqBody.providerDialogId === 'viber_service') {
+						let type = reqBody.body.elementType
+						console.log('\n\n\n viber message type : ', type)
+						if (type === 'image') {
+							let imageUrl = reqBody.attachments[0].url
+							let vonageResp = await sendVonageWhatsappImage(
+								recipiant,
+								imageUrl
+							)
+							console.log(
+								'vonage image resp--> ',
+								vonageResp.data
+							)
+						} else if (type === 'file') {
+							let fileUrl = reqBody.attachments[0].url
+							let vonageResp = await sendVonageWhatsappFile(
+								recipiant,
+								fileUrl
+							)
+							console.log('vonage file resp--> ', vonageResp.data)
+						} else {
+							let vonageResp = await sendVonageViberText(
+								recipiant,
+								replyMsg
+							)
+							console.log('vonage resp--> ', vonageResp.data)
+						}
+					} else {
+						// let smsResp = await sendSMS(recipiant, replyMsg);
+						// console.log("sms resp--> ", smsResp.data);
+					}
+				}
+			} else if (reqBody.senderParticipantType === 'CUSTOMER') {
+				console.log('customer msg --> ', reqBody.body.elementText.text)
+			}
+		}
+		res.send('callback url working')
+	} catch (error) {
+		console.log('error in callback---> ', error)
+		res.send(error)
+	}
+})
 
-app.post("/send-message", async (req, res) => {
-  console.log("send message called");
-  try {
-    let { sender, message, mobileNo, channel } = req.body;
-    console.log(sender, message, mobileNo);
-    let tokenResp = await sendMessage(sender, message, mobileNo, channel);
-    res.send(tokenResp);
-  } catch (error) {
-    console.log("Error in vonage-callback========> ", error.detail);
-    res.send(error);
-  }
-});
+app.post('/send-message', async (req, res) => {
+	console.log('send message called')
+	try {
+		let { sender, message, mobileNo, channel } = req.body
+		console.log(sender, message, mobileNo)
+		let tokenResp = await sendMessage(sender, message, mobileNo, channel)
+		res.send(tokenResp)
+	} catch (error) {
+		console.log('Error in vonage-callback========> ', error.detail)
+		res.send(error)
+	}
+})
 
-app.post("/vonage-callback", async (req, res) => {
-  console.log("POST vonage-callback");
-  console.log(req.body);
-  try {
-    let {
-      profile,
-      text,
-      from,
-      channel,
-      message_type,
-      image,
-      audio,
-      video,
-      file,
-      message_uuid,
-      location,
-      context,
-    } = req.body;
+app.post('/vonage-callback', async (req, res) => {
+	console.log('POST vonage-callback')
+	console.log(req.body)
+	try {
+		let {
+			profile,
+			text,
+			from,
+			channel,
+			message_type,
+			image,
+			audio,
+			video,
+			file,
+			message_uuid,
+			location,
+			context,
+		} = req.body
 
-    console.log(
-      "\n profile.name=>",
-      profile?.name,
-      "\n text=>",
-      text,
-      "\n from=>",
-      from,
-      "\n channel=>",
-      channel,
-      "\n image=>",
-      image,
-      "\n audio=>",
-      audio,
-      "\n video=>",
-      video,
-      "\n file=>",
-      file,
-      "\n message_uuid=>",
-      message_uuid,
-      "\n locatio=>",
-      location
-    );
+		console.log(
+			'\n profile.name=>',
+			profile?.name,
+			'\n text=>',
+			text,
+			'\n from=>',
+			from,
+			'\n channel=>',
+			channel,
+			'\n image=>',
+			image,
+			'\n audio=>',
+			audio,
+			'\n video=>',
+			video,
+			'\n file=>',
+			file,
+			'\n message_uuid=>',
+			message_uuid,
+			'\n locatio=>',
+			location
+		)
 
-    let sender;
+		let sender
 
-    if (channel === "viber_service") {
-      sender = context.message_from;
-    } else {
-      sender = profile.name;
-    }
+		if (channel === 'viber_service') {
+			sender = context.message_from
+		} else {
+			sender = profile.name
+		}
 
-    let fileDetails = undefined;
-    let locationDetails = undefined;
-    if (
-      message_type === "image" ||
-      message_type === "audio" ||
-      message_type === "file" ||
-      message_type === "video"
-    ) {
-      console.log("here=============================");
-      let resourceFile = image
-        ? image
-        : audio
-        ? audio
-        : video
-        ? video
-        : file
-        ? file
-        : undefined;
-      fileDetails = await uploadFileToAvaya(resourceFile);
-    } else if (message_type === "location") {
-      locationDetails = location;
-    }
+		let fileDetails = undefined
+		let locationDetails = undefined
+		if (
+			message_type === 'image' ||
+			message_type === 'audio' ||
+			message_type === 'file' ||
+			message_type === 'video'
+		) {
+			console.log('here=============================')
+			let resourceFile = image
+				? image
+				: audio
+					? audio
+					: video
+						? video
+						: file
+							? file
+							: undefined
+			fileDetails = await uploadFileToAvaya(resourceFile)
+		} else if (message_type === 'location') {
+			locationDetails = location
+		}
 
-    console.log("fileDetails================> ", fileDetails);
-    let tokenResp = await sendMessage(
-      sender,
-      text,
-      from,
-      channel,
-      message_type,
-      fileDetails,
-      locationDetails
-    );
-    res.send(tokenResp);
-  } catch (error) {
-    console.log("Error in vonage-callback========> ", error);
-    res.send(error);
-  }
-});
+		console.log('fileDetails================> ', fileDetails)
+		let tokenResp = await sendMessage(
+			sender,
+			text,
+			from,
+			channel,
+			message_type,
+			fileDetails,
+			locationDetails
+		)
+		res.send(tokenResp)
+	} catch (error) {
+		console.log('Error in vonage-callback========> ', error)
+		res.send(error)
+	}
+})
 
-app.post("/line-callback", async (req, res) => {
-  console.log("POST line-callback");
-  console.log(JSON.stringify(req.body));
-  try {
-    let { events } = req.body;
-    let fileDetails = undefined;
-    let locationDetails = undefined;
+app.post('/line-callback', async (req, res) => {
+	console.log('POST line-callback')
+	console.log(JSON.stringify(req.body))
+	try {
+		let { events } = req.body
+		let fileDetails = undefined
+		let locationDetails = undefined
 
-    if (events.length > 0) {
-      let messageEvent = events[0];
-      let messageType = messageEvent.message.type;
-      console.log("MessageType : " + messageType);
+		if (events.length > 0) {
+			let messageEvent = events[0]
+			let messageType = messageEvent.message.type
+			console.log('MessageType : ' + messageType)
 
-      if (messageType === "text") {
-        let tokenResp = await sendMessage(
-          messageEvent.source.type,
-          messageEvent.message.text,
-          messageEvent.source.userId,
-          "Line",
-          messageType,
-          fileDetails,
-          locationDetails
-        );
-        res.send(tokenResp);
-      } else if (messageType === "image") {
-        console.log("Image Message type not supported.");
-      } else if (messageType === "location") {
-        locationDetails = {
-          lat: messageEvent.message.latitude,
-          long: messageEvent.message.longitude,
-        };
-        let tokenResp = await sendMessage(
-          messageEvent.source.type,
-          messageEvent.message.address,
-          messageEvent.source.userId,
-          "Line",
-          messageType,
-          fileDetails,
-          locationDetails
-        );
-      } else if (messageType === "audio") {
-        console.log("Audio Message type not supported.");
-      } else if (messageType === "video") {
-        console.log("Video Message type not supported.");
-      } else if (messageType === "sticker") {
-        console.log("Sticker Message type not supported.");
-      } else {
-        console.log("Message type not supported.");
-      }
-    }
-  } catch (error) {
-    console.log("line-callback error========> ", error.detail);
-    res.send(error);
-  }
-  res.send("OK");
-});
+			if (messageType === 'text') {
+				let tokenResp = await sendMessage(
+					messageEvent.source.type,
+					messageEvent.message.text,
+					messageEvent.source.userId,
+					'Line',
+					messageType,
+					fileDetails,
+					locationDetails
+				)
+				res.send(tokenResp)
+			} else if (messageType === 'image') {
+				console.log('Image Message type not supported.')
+			} else if (messageType === 'location') {
+				locationDetails = {
+					lat: messageEvent.message.latitude,
+					long: messageEvent.message.longitude,
+				}
+				let tokenResp = await sendMessage(
+					messageEvent.source.type,
+					messageEvent.message.address,
+					messageEvent.source.userId,
+					'Line',
+					messageType,
+					fileDetails,
+					locationDetails
+				)
+			} else if (messageType === 'audio') {
+				console.log('Audio Message type not supported.')
+			} else if (messageType === 'video') {
+				console.log('Video Message type not supported.')
+			} else if (messageType === 'sticker') {
+				console.log('Sticker Message type not supported.')
+			} else {
+				console.log('Message type not supported.')
+			}
+		}
+	} catch (error) {
+		console.log('line-callback error========> ', error.detail)
+		res.send(error)
+	}
+	res.send('OK')
+})
 
-app.post("/viber-callback", async (req, res) => {
-  console.log("POST viber-callback");
-  console.log(JSON.stringify(req.body));
-  res.send("OK");
-});
+app.post('/viber-callback', async (req, res) => {
+	console.log('POST viber-callback')
+	console.log(JSON.stringify(req.body))
+	res.send('OK')
+})
